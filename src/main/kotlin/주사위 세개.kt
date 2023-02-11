cdc7d8cdf08c16bbe27bdf89f2ec2956da71b5c4
joh9911 <joh9911@naver.com>
2023-02-11 17:49:57 +0900

    problem

---
+++ src/main/kotlin/주사위 세개.kt
@@ -0,0 +1,33 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (a,b,c) = br.readLine().split(' ').map { it.toInt() }
+    calculator(a,b,c)
+}
+fun calculator(a: Int, b: Int, c: Int){
+    var prize = 0
+    if (a == b && a == c){
+        prize = 10000 + a*1000
+        println(prize)
+    }
+    else if (a != b && a != c && c != b){
+        val list = arrayListOf(a, b, c)
+        prize = list.max() * 100
+        println(prize)
+    }
+    else{
+        val list = arrayListOf(a, b, c)
+        val distinctList = arrayListOf(a, b, c).distinct()
+
+        for (index in distinctList.indices){
+            if (list.contains(distinctList[index]))
+                list.remove(distinctList[index])
+        }
+        prize = 1000 + (list[0]*100)
+        println(prize)
+    }
+
+}
+
