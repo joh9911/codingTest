fc5ff3afd2890b4ebb7cdbdae2f4091a99b07ade
joh9911 <joh9911@naver.com>
2023-02-13 00:59:06 +0900

    solved

--- src/main/kotlin/secondDay/크로아티아 알파벳 2941번.kt
+++ src/main/kotlin/secondDay/크로아티아 알파벳 2941번.kt
@@ -11,16 +11,24 @@ fun main(){
     mp["s="] = "š"
     mp["z="] = "ž"
     val br = BufferedReader(InputStreamReader(System.`in`))
-    var word = br.readLine()
-    val list = mutableListOf<String>()
-    var count = 0
-    mp.forEach{ m->
-        if(word.contains(m.key)){
-            var trash = word.split(m.key)
-            println(trash)
-            println(trash.size-1)
-            println(m.value.count())
-            count += (trash.size-1)*(m.value.count())
+    var word = br.readLine() //입력 받은 문자열
+    var count = 0 // count
+    var tag = -1 // 문자를 하나하나씩 검사할 거임
+    for (index in word.indices){ // 한 단어씩 이동
+        var ch = ""
+        if (index <= tag){ //검사한 문자열까지 index를 설정하기 위해 만든 조건문
+            continue
+        }
+        for (index1 in index until word.length){ // 한 단어에서 게속해서 string 붙힘
+            ch += word[index1]
+            if (mp.keys.contains(ch)){ //붙힌 문자열이 key에 포함되었을 경우
+                count += 1 // 횟수 증가
+                tag = index1 // 그 인덱스에 태그 설정
+                break
+            }
+            if (index1 == word.length-1){ // 계속해서 문자열을 붙혀도 key에 없는 경우 알파벳 하나로 침
+                count++
+            }
         }
     }
     println(count)
