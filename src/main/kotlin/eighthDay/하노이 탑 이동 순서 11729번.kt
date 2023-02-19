438fda6a66ab2594cc76fb68c6c94bb295071adf
joh9911 <joh9911@naver.com>
2023-02-19 23:31:21 +0900

    solved

--- src/main/kotlin/eighthDay/하노이 탑 이동 순서 11729번.kt
+++ src/main/kotlin/eighthDay/하노이 탑 이동 순서 11729번.kt
@@ -2,20 +2,22 @@ package eighthDay
 import java.io.*
 import java.util.*
 
-fun main(){
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val n = br.readLine().toInt()
-    val arr = Vector<Vector<Int>>()
-    for (index in 0 until n){
-        arr[0][index] = index+1
-    }
-    val count = 0
-    func(count, arr)
-}
-fun func(count: Int, arr: Vector<Vector<Int>>){
-    for (index in arr.indices){
-        if (arr[index])
-    }
+fun main() {
+    val sb = StringBuilder()
+    val n = readLine()!!.toInt()
+    var cnt = 0
 
-
+    fun hanoi(num: Int, start: Int, sub: Int, to: Int){
+        if (num == 0) return
+        cnt += 1
+        hanoi (num - 1, start, to, sub) // N - 1 개 원반을 시작점 -> sub
+        sb.append("$start $to\n") // n번 째 원반 -> 목표 기둥
+        hanoi(num - 1, sub, start, to) // N-1 개 원반 sub -> to
+    }
+    hanoi(n, 1,2,3)
+    println(cnt)
+    print(sb)
 }
+// 남의 코드 배낀거임
+// 아예 재귀 호출 방식을 제대로 공부해보자
+// 풀이 봐도 감이 안온다
