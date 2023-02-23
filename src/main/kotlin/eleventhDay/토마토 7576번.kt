c26bf364813d96d17f5fc84c2746eeb2d222e572
joh9911 <joh9911@naver.com>
2023-02-23 22:20:44 +0900

    solved

--- src/main/kotlin/eleventhDay/토마토 7576번.kt
+++ src/main/kotlin/eleventhDay/토마토 7576번.kt
@@ -1,57 +1,49 @@
 package eleventhDay
 import java.io.*
+import java.util.*
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (m, n) = br.readLine().split(' ').map { it.toInt() }
     val arr = Array(n) { Array(m) { 0 } }
+    val dx = arrayOf(0, 0, 1, -1)
+    val dy = arrayOf(1, -1, 0, 0)
+    val addList = LinkedList<Pair<Int,Int>>()
     repeat(n) { i ->
         val input = br.readLine().split(' ').map { it.toInt() }
         for (index in input.indices) {
+            if (input[index] == 1)
+                addList.add(Pair(i,index))
             arr[i][index] = input[index]
         }
     }
-    var count = 0
-    var temp = 0
-    val addList = mutableListOf<Pair<Int,Int>>()
-    fun func(num: Int, count: Int){
-        if (addList.isEmpty())
-            return
-    }
-    while (true) {
-        for (index in arr.indices) {
-            for (index1 in arr[index].indices) {
-                if (arr[index][index1] == 1) {
-                    if (index1 - 1 >= 0) {
-                        if (arr[index][index1 - 1] != 1 && arr[index][index1 - 1] != -1)
-                            addList.add(Pair(index,index1-1))
-                    }
-                    if (index1 + 1 <= arr[index].size - 1) {
-                        if (arr[index][index1 + 1] != 1 && arr[index][index1 + 1] != -1)
-                            addList.add(Pair(index,index1+1))
-                    }
-                    if (index - 1 >= 0) {
-                        if (arr[index - 1][index1] != 1 && arr[index - 1][index1] != -1)
-                            addList.add(Pair(index-1,index1))
-                    }
-                    if (index + 1 <= arr.size - 1) {
-                        if (arr[index + 1][index1] != 1 && arr[index + 1][index1] != -1)
-                            addList.add(Pair(index+1,index1))
-                    }
+    fun bfs(): Int{
+        var count = 0
+        while (addList.size != 0 ){
+            val num = addList.poll()
+            for (index in dx.indices){
+                val numX = num.second + dx[index]
+                val numY = num.first + dy[index]
+                if (numX < 0 || numX >= m || numY < 0 || numY >= n)
+                    continue
+                if (arr[numY][numX] != 0)
+                    continue
+                arr[numY][numX] = arr[num.first][num.second] + 1
+                addList.add(Pair(numY,numX))
+            }
+
+        }
+
+        for (index in arr.indices){
+            for (index1 in arr[index].indices){
+                if (arr[index][index1] == 0){
+                    return -1
                 }
-                if (arr[index][index1] == 0)
-                    temp += 1
+                count = Math.max(count, arr[index][index1])
             }
         }
-        if (addList.isEmpty())
-            break
-        addList.forEach{arr[it.first][it.second] = 1}
-        addList.clear()
-        temp = 0
-        count += 1
+        return count-1
     }
-    if (temp == 0)
-        println(count)
-    else
-        println(-1)
+
+    println(bfs())
 
 }
