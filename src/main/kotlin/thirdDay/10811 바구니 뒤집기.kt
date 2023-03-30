bdcfe11be1d0d2767eb9f3a5420cd21ed74ec866
joh9911 <joh9911@naver.com>
2023-03-30 22:15:41 +0900

    solved

---
+++ src/main/kotlin/thirdDay/10811 바구니 뒤집기.kt
@@ -0,0 +1,18 @@
+package thirdDay
+import java.io.*
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (n, m) = br.readLine().split(' ').map{it.toInt()}
+    val list = mutableMapOf<Int,Int>()
+    for (index in 1..n){
+        list[index] = index
+    }
+    repeat(m){
+        val (i, j) = br.readLine().split(' ').map{it.toInt()}
+        for (index in j until i){
+            
+        }
+    }
+}
+
---
+++ src/main/kotlin/피보나치 수.kt
@@ -0,0 +1,14 @@
+import java.io.*
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+
+    fun fib(num: Int): Int {
+        if (num == 0)
+            return 0
+        if (num == 1)
+            return 1
+        return fib(num - 1) + fib(num - 2)
+    }
+    println(fib(n))
+}
