a73a4971451f170bcd37f514bc07432315bd589d
joh9911 <joh9911@naver.com>
2023-05-25 22:42:08 +0900

    solved

--- src/main/kotlin/fiftyfourth/2503 숫자 야구.kt
+++ src/main/kotlin/fiftyfourth/2503 숫자 야구.kt
@@ -1,56 +1,70 @@
 package fiftyfourth
 // 10:55
 import java.io.*
-fun main(){
+fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
-    val arr = arrayListOf<List<Char>>()
-    repeat(n){
-        val (input, s, b) = br.readLine().split(' ').map{it.toInt()}
-        val number = input.toString()
-        if (s == 1){
-            for (i in 0 until 3){
-                val string = Array(3){' '}
-                string[i] = number[i]
-                if (b == 1){
-                    val temp = arrayListOf<Char>()
-                    for (j in 0 until 3){
-                        if (j == i) continue
-                        temp.add(number[j])
-                    }
-                    var l = 0
-                    temp.reverse()
-                    println(temp)
-                    for (k in 0 until 3){
-                        if (string[k] == ' ') {
-                            string[k] = temp[l]
-                            arr.add(string.toList())
-                            string[k] = ' '
-                            l++
-                        }
-                    }
-                }
-                else if (b == 2){
-                    val temp = arrayListOf<Char>()
-                    for (j in 0 until 3){
-                        if (j == i) continue
-                        temp.add(number[j])
-                    }
-                    var l = 0
-                    temp.reverse()
-                    for (k in 0 until 3){
-                        if (string[k] == ' '){
-                            string[k] = temp[l]
-                            l++
-                        }
-                    }
-                    arr.add(string.toList())
-                }
+
+    data class Game(
+            val number: String,
+            val strike: Int,
+            val ball: Int
+    )
+
+    val gameArr = Array(n){Game("",0,0)}
+
+    repeat(n) {
+        val (number, strike, ball) = br.readLine().split(' ').map { it.toInt() }
+        gameArr[it] = Game(number.toString(),strike,ball)
+    }
+
+    val ans = Array(3){0}
+    val visit = Array(10){false}
+
+
+    fun check(arr: Array<Int>): Boolean{
+        for (index in gameArr.indices){
+            val game = gameArr[index]
+            var number = game.number
+
+            var sCount = 0
+            for (i in arr.indices){
+                if (arr[i] == number[i].toString().toInt())
+                    sCount++
+            }
+            if (sCount != game.strike)
+                return false
+
+            var bCount = 0
+            for (i in arr.indices){
+                if (arr[i] == number[(i+1)%3].toString().toInt())
+                    bCount ++
+                if (arr[i] == number[(i+2)%3].toString().toInt())
+                    bCount ++
+            }
+            if (bCount != game.ball)
+                return false
+        }
+        return true
+    }
+    var ansCount = 0
+
+    fun search(num: Int){
+        if (num == 3){
+            if (check(ans))
+                ansCount ++
+            return
+        }
+        for (index in 1..9){
+            if (!visit[index]){
+                visit[index] = true
+                ans[num] = index
+                search(num+1)
+                visit[index] = false
             }
         }
-        else if (s == 2){
-
-        }
     }
+    search(0)
+    println(ansCount)
 
 }
